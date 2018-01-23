a64fe598f2c39a83e9f49e4cc6c580df6a93a2d2
Sam <sgluss@mail.sfsu.edu>
2018-01-23 00:34:16 -0800

    testing changes

--- lambci.sh
+++ lambci.sh
@@ -1,5 +1,5 @@
 echo starting build script
 pip install --user s3cmd
-echo *** pip installed! ***
+echo "\n*** pip installed! ***\n"
 s3cmd put ./artifacts/syncme s3://hugosite-artifacts
 echo build script complete
