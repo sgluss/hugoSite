ffc458252312504b34e972552cfa87ed7ccc5a30
Sam <sgluss@mail.sfsu.edu>
2018-01-23 20:15:32 -0800

    fun debug times with bacon

--- lambci.sh
+++ lambci.sh
@@ -2,4 +2,6 @@ echo "*** Starting build script ***"
 pip install awscli --upgrade --user
 echo "*** awscli installed! ***"
 aws s3 cp artifacts/syncme s3://hugosite-artifacts
+echo "*** Environment Variables***"
+printenv
 echo "*** Build script complete ***"
