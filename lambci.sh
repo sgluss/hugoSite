160521115409075ab660a17a0b4d7159768ee466
APOPHIS\sam <sgluss@outlook.com>
2018-01-25 23:37:31 -0800

    ok I guess not

--- lambci.sh
+++ lambci.sh
@@ -1,4 +1,6 @@
 echo "*** Starting build script ***"
+echo "*** awscli installing! ***"
+pip install awscli --upgrade --user
 
 echo "*** Installing Hugo! ***"
 npm install --prefix=~/lambdaTestFunction hugo-cli
@@ -9,5 +11,4 @@ echo "*** copying files to AWS S3! ***"
 aws s3 cp artifacts/syncme s3://hugosite-artifacts
 echo "*** Environment Variables***"
 printenv
-
 echo "*** Build script complete ***"
