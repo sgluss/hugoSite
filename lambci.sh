8ca73cb7d43f3e7364e6937e11240a15bc774abd
APOPHIS\sam <sgluss@outlook.com>
2018-01-25 22:57:51 -0800

    trying out installing hugo from source

--- lambci.sh
+++ lambci.sh
@@ -1,6 +1,15 @@
 echo "*** Starting build script ***"
+echo "*** awscli installing! ***"
 pip install awscli --upgrade --user
-echo "*** awscli installed! ***"
+
+echo "*** Getting Govendor! ***"
+go get github.com/kardianos/govendor
+echo "*** Getting Hugo! ***"
+govendor get github.com/gohugoio/hugo
+echo "*** Installing Hugo! ***"
+go install github.com/gohugoio/hugo
+
+echo "*** copying files to AWS S3! ***"
 aws s3 cp artifacts/syncme s3://hugosite-artifacts
 echo "*** Environment Variables***"
 printenv
