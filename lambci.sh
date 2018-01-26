96dbfc8d59d62003ca7341939fae24cc14ce8bd4
APOPHIS\sam <sgluss@outlook.com>
2018-01-25 23:41:14 -0800

    try a different angle... download the binary for hugo

--- lambci.sh
+++ lambci.sh
@@ -2,10 +2,14 @@ echo "*** Starting build script ***"
 echo "*** awscli installing! ***"
 pip install awscli --upgrade --user
 
-echo "*** Installing Hugo! ***"
-npm install --prefix=~/lambdaTestFunction hugo-cli
+echo "*** Downloading Hugo! ***"
+mkdir hugo && cd hugo
+wget https://github.com/gohugoio/hugo/releases/download/v0.34/hugo_0.34_Linux-64bit.tar.gz
+echo "*** Unzipping Hugo! ***"
+gunzip hugo_0.34_Linux-64bit.tar.gz
 echo "*** Verifying Hugo! ***"
 hugo -h
+cd ..
 
 echo "*** copying files to AWS S3! ***"
 aws s3 cp artifacts/syncme s3://hugosite-artifacts
