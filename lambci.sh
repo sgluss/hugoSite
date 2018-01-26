72377be7ff7f4ff080eff47d016a4eb9807f64bd
APOPHIS\sam <sgluss@outlook.com>
2018-01-25 23:36:52 -0800

    seems lambda already has AWS SDK built in, slick

--- lambci.sh
+++ lambci.sh
@@ -1,6 +1,4 @@
 echo "*** Starting build script ***"
-echo "*** awscli installing! ***"
-pip install awscli --upgrade --user
 
 echo "*** Installing Hugo! ***"
 npm install --prefix=~/lambdaTestFunction hugo-cli
@@ -11,4 +9,5 @@ echo "*** copying files to AWS S3! ***"
 aws s3 cp artifacts/syncme s3://hugosite-artifacts
 echo "*** Environment Variables***"
 printenv
+
 echo "*** Build script complete ***"
