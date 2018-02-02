c6aab3d62040317ad857af718a0ed4dfebbd3aac
APOPHIS\sam <sgluss@outlook.com>
2018-02-01 21:08:38 -0800

    trying bundling hugo into the lambda itself

--- lambci.sh
+++ lambci.sh
@@ -2,9 +2,9 @@ echo "*** Starting build script ***"
 echo "*** awscli installing! ***"
 pip install awscli --upgrade --user
 
-echo "*** Installing Hugo! ***"
-curl https://github.com/gohugoio/hugo/releases/download/v0.34/hugo_0.34_Linux-64bit.tar.gz | tar xvz
+echo "*** Testing Packaged Hugo! ***"
 ls
+./hugo -h
 
 echo "*** Verifying Hugo! ***"
 hugo -h
