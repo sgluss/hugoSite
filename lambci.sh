35c994c006ec242b62235a40d92af1e2acb72d29
APOPHIS\sam <sgluss@outlook.com>
2018-02-11 11:30:18 -0800

    testing moving hugo to tmp

--- lambci.sh
+++ lambci.sh
@@ -4,6 +4,18 @@ echo "*** Checking /var/task/ ***"
 cd /var/task/
 ls
 
+echo "copying hugo to tmp"
+cp -R /var/task/hugo /tmp/
+
+echo "going to tmp hugo dir"
+cd /tmp
+echo "contents of tmp"
+ls
+cd /tmp/hugo
+
+echo "what's in hugo?"
+ls
+
 echo "*** Verifying Hugo! ***"
 hugo -h
 cd ..
